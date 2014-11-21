f3212d1158427c3fd30ec1b1c5450d126455df6e
Mka Madlavana <mka@takealot.com>
2014-11-21 21:46:45 +0200

    Added a Zeckendorf Representation generator

--- mathutils/utils.py
+++ mathutils/utils.py
@@ -1,4 +1,4 @@
-""" Fibonacci number generator. """
+""" Some math utilities """
 
 from collections import deque
 
@@ -10,5 +10,26 @@ def fibonacci():
 
     fibs = deque(maxlen=2)
     while True:
-        fibs.append(sum(fibs) if len(fibs) >= 2 else len(fibs) + 1)
+        fibs.append(sum(fibs) if len(fibs) == 2 else len(fibs) + 1)
         yield fibs[-1]
+
+
+def zeckendorf(number):
+    """
+    Generates Zeckendorf's representation of the given number
+    """
+
+    fibonacci_numbers = []
+    for fib in fibonacci():
+        if fib > number:
+            break
+        fibonacci_numbers.append(fib)
+
+    zeckendorf_numbers = []
+    candidate = sum(zeckendorf_numbers)
+    while candidate < number:
+        for fib in reversed(fibonacci_numbers):
+            if candidate + fib <= number and fib not in zeckendorf_numbers:
+                zeckendorf_numbers.append(fib)
+                candidate = sum(zeckendorf_numbers)
+    return zeckendorf_numbers
