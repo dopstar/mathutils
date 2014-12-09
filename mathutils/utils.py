72bc860523a3fa1b0b9cccb11ee97d658ee3d9e3
mka <mka@takealot.com>
2014-12-10 00:32:50 +0200

    Added returning Zeckendorf Representation as a sequence of binary digits

--- mathutils/utils.py
+++ mathutils/utils.py
@@ -1,6 +1,6 @@
 """ Some math utilities """
 
-from collections import deque
+from collections import deque, Counter
 
 
 def fibonacci():
@@ -27,7 +27,7 @@ def is_fibonacci(number):
     return is_fib
 
 
-def zeckendorf(number):
+def zeckendorf(number, binary=False):
     """
     Generates Zeckendorf's representation of the given number
     """
@@ -39,10 +39,15 @@ def zeckendorf(number):
         fibonacci_numbers.append(fib)
 
     zeckendorf_numbers = []
+    zeckendorf_representation = Counter()
     candidate = sum(zeckendorf_numbers)
     while candidate < number:
         for fib in reversed(fibonacci_numbers):
             if candidate + fib <= number and fib not in zeckendorf_numbers and fib > 0:
                 zeckendorf_numbers.append(fib)
+                zeckendorf_representation[fib] = 1
                 candidate = sum(zeckendorf_numbers)
+    if binary:
+        sequence = [str(zeckendorf_representation[fib]) for fib in reversed(fibonacci_numbers)]
+        return ''.join(sequence)
     return zeckendorf_numbers
