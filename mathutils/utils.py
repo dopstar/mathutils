e200f4b979d01080086e0679cbc3a47046ca2603
mka <mka@takealot.com>
2015-01-02 01:05:16 +0200

    Added the Golden Ratio, the Binet's Formula implementation and the Zeckendorf Representation conversion function.

--- mathutils/utils.py
+++ mathutils/utils.py
@@ -1,6 +1,9 @@
 """ Some math utilities """
 
+import math
 from collections import deque, Counter
+
+from constants import gr
 
 
 def fibonacci():
@@ -36,7 +39,8 @@ def zeckendorf(number, binary=False):
     for fib in fibonacci():
         if fib > number:
             break
-        fibonacci_numbers.append(fib)
+        if fib > 0 and fib not in fibonacci_numbers:
+            fibonacci_numbers.append(fib)
 
     zeckendorf_numbers = []
     zeckendorf_representation = Counter()
@@ -51,3 +55,32 @@ def zeckendorf(number, binary=False):
         sequence = [str(zeckendorf_representation[fib]) for fib in reversed(fibonacci_numbers)]
         return ''.join(sequence)
     return zeckendorf_numbers
+
+
+def binet(n):
+    """
+    Binet's Formula
+    :param n: integer number
+    :return: the n-th Fibonacci number
+    """
+    return int((gr**n - (-gr)**(-n)) / math.sqrt(5))
+
+
+def F(n):
+    """
+    Alias for binet()
+    """
+    return binet(n)
+
+
+def zeckendorf_to_decimal(representation):
+    """
+    Converts Zeckendorf representation binary sequence into a decimal integer
+    :param representation: Zeckendorf representation binary sequence string.
+    :return: integer.
+    """
+
+    number = 0
+    for n, digit in zip(xrange(len(representation) + 1, 1, -1), (int(r) for r in representation)):
+        number += digit * F(n)
+    return number
